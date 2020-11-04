30acca004609a3e8d7d9de647d8d2c1a168cf3c4
samekhardt <58480041+samekhardt@users.noreply.github.com>
2020-11-04 16:43:01 -0500

    Update to commands DM

--- commands/commands.js
+++ commands/commands.js
@@ -6,5 +6,6 @@ module.exports = {
        message.author.send("-checkage , this will show how long you've been in this server");
        message.author.send("-leaderboard , this will send a leaderboard of who has been in the sever the longest");
        message.author.send("-roles , this will show the current roles available in the server");
+       message.author.send("-giveme, this command will pull random image from search based on what is typed after the command. EX: -giveme Siege");
     }
 };
